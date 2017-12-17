af063995c02642418ef4ff5232d72fa5fc1f8864
Sergiu Bologa <sergiu.bologa@paddypowerbetfair.com>
2017-12-17 22:55:21 +0200

    Initial commit

---
+++ src/routes/users.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.json([{
+  	id: 1,
+  	username: "sergiu"
+  }, {
+  	id: 2,
+  	username: "bia"
+  }]);
+});
+
+module.exports = router;
