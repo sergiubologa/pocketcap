41d346f1f5146fd9a90b62b40e94a9f5439f8fcf
Sergiu Bologa <sergiu.bologa@paddypowerbetfair.com>
2018-01-03 22:13:37 +0200

    Integrate Flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
+// @flow
 import React, { Component } from 'react';
 import moment from 'moment';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  state = {coins: {added_at: new Date().toString(), data: []}};
+type Coin = {
+  id: string,
+  name: string,
+  symbol: string,
+  rank: number,
+  price_usd: number,
+  price_btc: number,
+  "24h_volume_usd": number,
+  market_cap_usd: number,
+  available_supply: number,
+  total_supply: number,
+  max_supply: number,
+  percent_change_1h: number,
+  percent_change_24h: number,
+  percent_change_7d: number,
+  last_updated: number
+};
+
+type Props = {};
+
+type State = {
+  coins: { added_at: ?Date, data: Array<Coin> }
+};
+
+class App extends Component<Props, State> {
+  state = {
+    coins: { added_at: undefined, data: [] }
+  };
 
   componentDidMount() {
     console.log('fetch coins')
@@ -15,12 +42,15 @@ class App extends Component {
       }
     })
     .then(res => {
-      console.log(res)
+      console.log(res);
       return res.json();
     })
     .then(coins => {
       console.log(coins)
       this.setState({coins})
+    })
+    .catch((err) => {
+      console.log(err);
     });
   }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement: ?HTMLElement = (document.getElementById('root'): ?HTMLElement);
+
+if (rootElement == null) {
+  throw new Error('Root element is missing!');
+}
+
+ReactDOM.render(<App />, rootElement);
 registerServiceWorker();
