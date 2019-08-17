e1396aaaad77749da9e0a8f3019d5e55b7e7e396
Sergiu Bologa <sergiu.calin.bologa@gmail.com>
2019-08-17 10:47:57 +0300

    Upgrade market stats to latest API version

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
-require('dotenv').config();
-require('checkenv').check();
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const { coinsCronJob } = require('cron-jobs/coins-cron');
-const { marketStatsCronJob } = require('cron-jobs/market-stats-cron');
-const { cleanDBCronJob } = require('cron-jobs/clean-db-cron');
+require("dotenv").config();
+require("checkenv").check();
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const { coinsCronJob } = require("cron-jobs/coins-cron");
+const { marketStatsCronJob } = require("cron-jobs/market-stats-cron");
+const { cleanDBCronJob } = require("cron-jobs/clean-db-cron");
 
-const index = require('routes/index');
-const coins = require('routes/coins');
-const marketStats = require('routes/market-stats');
-const allOthers = require('routes/all-others');
+const index = require("routes/index");
+const coins = require("routes/coins");
+const marketStats = require("routes/market-stats");
+const allOthers = require("routes/all-others");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Delay responses to simulate latency
 //app.use(function(req,res,next){setTimeout(next,2000)});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', index);
-app.use('/api/coins', coins);
-app.use('/api/market-stats', marketStats);
-app.use('*', allOthers);
+// Start API
+app.use("/api", index);
+app.use("/api/coins", coins);
+app.use("/api/market-stats", marketStats);
+app.use("*", allOthers);
+console.log(`Started server on port ${process.env.PORT}`);
 
 // Start cron jobs
-if (process.env.NODE_ENV != 'testing'){
-    coinsCronJob.start();
-    marketStatsCronJob.start();
-    cleanDBCronJob.start();
-    // TODO - use logger
-    console.log('Cron jobs started');
-    //setInterval(() => console.log(coinsCronJob.nextDates(), process.env.NODE_ENV), 1000);
+if (process.env.NODE_ENV != "testing") {
+  coinsCronJob.start();
+  marketStatsCronJob.start();
+  cleanDBCronJob.start();
+  // TODO - use logger
+  console.log("Cron jobs started");
+  //setInterval(() => console.log(coinsCronJob.nextDates(), process.env.NODE_ENV), 1000);
 }
 
 // // catch 404 and forward to error handler
